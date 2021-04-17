b22eab750cc354a891490f26a160af93bdf4cb80
Damion <dollardamion@gmail.com>
2021-04-16 20:12:31 -0400

    next work on graphics

--- todo.java
+++ todo.java
@@ -1,24 +1,55 @@
 import java.util.*;
-public class todo{
-ArrayList<String>list=new ArrayList<>();
-public static void main(String[]args){
 
+public class todo{
+ private static ArrayList<String>list=new ArrayList<>();
+public static void main(String[]args){
+   Scanner kb = new Scanner(System.in);
+   String choice="";
+   
+  
+            System.out.println( "Welcome to TODO, here are your options");
+            while(!choice.equals("-1")){
+            
+            System.out.println( "1.add task to list"+"\n"+"2.remove task from list"+"\n"+"3.view list"+"\n");
+            choice =kb.nextLine();
+
+           
+      
+            switch(choice) {
+                
+                case "1": 
+                String a=kb.nextLine();
+                add(a);
+                break;
+               case "2":
+               String b=kb.nextLine();
+                    remove(b);             
+               break;
+                case "3":
+                view();
+                break;
+            }
+            
+            }
 }
 
 public  static void view(){
-String[]a=list.toArray();
+Object[]a=list.toArray();
 for(int i=0;i<a.length;i++){
-System.out.println(a[i]);
-}
+System.out.println(i+1+"."+a[i] );
+System.out.println();
+
 
 }
 
-public  static List<String> add(String c){
+}
+
+public  static void add(String c){
 list.add(c);
 
 }
 
-public  static List<String> remove(String c){
+public  static void remove(String c){
 list.remove(c);
 
 }
